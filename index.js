b79b4a54d6d0a611fb886010c956b7c1105ec65a
krgarima <kumargarima59@gmail.com>
2021-09-20 17:37:05 +0530

    updated logic
    lucky number negative error

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const resultLucky = document.querySelector("#result");
 checkButton.addEventListener("click", function checkLuckyNumber() {
     const doob = dateOfBirth.value;
     const sum1 = dobSum(doob);
-
-    if (sum1 && doob) {
-        if ((sum1 % luckyNumber.value) === 0)
-            resultLucky.innerText = "Voila! Your birthday is Lucky 😮";
-        else
-            resultLucky.innerText = "Sorry! Your birthday is not a Lucky number 🤕";
+    if ((Number(luckyNumber.value)) && (doob)) {
+        if (Number(luckyNumber.value) > 0) {
+            if ((sum1 % luckyNumber.value) === 0)
+                resultLucky.innerText = "Voila! Your birthday is Lucky 😮";
+            else
+                resultLucky.innerText = "Sorry! Your birthday is not a Lucky number 🤕";
+        } else
+            resultLucky.innerText = "Lucky number entered is Negative!";
     } else
         resultLucky.innerText = "Please enter both the fields 🥺"
 });
@@ -20,7 +22,7 @@ function dobSum(number) {
     let sum2 = 0;
     const dob = number.replaceAll("-", "");
     for (let i = 0; i < dob.length; i++) {
-        sum2 = sum2 + dob.charAt(i);
+        sum2 = sum2 + Number(dob.charAt(i));
     }
     return sum2;
 }
